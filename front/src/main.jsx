748ca781d6ec7d146896e248d5b242afbfe5ba7c
Maël V <maelv.contact@gmail.com>
2025-08-17 22:16:56 +0200

    changement sécurité

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 
-// Créer une instance de QueryClient
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
